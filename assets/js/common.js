12232588998845a1c143fc44df363dbc40af8897
gitaeks <break@kimsq.com>
2015-11-11 03:15:56 +0900

    개선

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,3 +5,31 @@ $('body').tooltip({
 	html: 'true',
 	container: 'body',
 });
+
+// feedback messages
+var feedback = (function() {
+    "use strict";
+
+    var elem,
+        hideHandler,
+        that = {};
+
+    that.init = function(options) {
+        elem = $(options.selector);
+    };
+
+    that.show = function(text) {
+        clearTimeout(hideHandler);
+
+        elem.find("span").html(text);
+        elem.delay(100).fadeIn().delay(500).fadeOut();
+    };
+
+    return that;
+}());
+
+$(function () {
+    feedback.init({
+        "selector": ".rb-feedback "
+    });
+});
