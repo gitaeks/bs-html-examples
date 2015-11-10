0781c7dcd75a4b9b7e65df6515e94f73b6fc18c4
gitaeks <break@kimsq.com>
2015-11-11 05:24:17 +0900

    이미지 자동 캡션 추출기능 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -33,3 +33,29 @@ $(function () {
         "selector": ".rb-feedback "
     });
 });
+
+
+
+// jQuery.imageCaption.js : https://github.com/andreasnymark/jQuery.imageCaption
+
+(function($){ 
+	$.fn.imageCaption = function(options) {
+		var defaults = {  
+		    figure: '<figure>',
+		    figcaption: '<figcaption>'
+		};  
+		var options = $.extend(defaults, options); 
+		return this.each(function() { 
+			var o = $(this);
+			var alt = o.attr('alt');
+			var classes = o.attr('class');
+			var caption = $(options.figcaption).text(alt);
+			o
+				.removeClass()
+				.wrap(options.figure)
+				.parent()
+				.addClass(classes)
+				.append(caption);
+		});
+	}
+})(jQuery);
